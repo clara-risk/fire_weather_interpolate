070323381df5841d04b464bb3959a4500de76d53
clara-risk <43723728+clara-risk@users.noreply.github.com>
2020-08-17 19:07:07 -0400

    add some code for spatial block k-folds

--- fire_weather_interpolate/idw.py
+++ fire_weather_interpolate/idw.py
@@ -230,3 +230,99 @@ def cross_validate_IDW(latlon_dict,Cvar_dict,shapefile,d):
 
      return absolute_error_dictionary
      
+def make_fold(idw_grid):
+     '''Function to create an array delineating the groups. 
+     '''
+     shape = idw_grid.shape
+     blocks = np.array_split(idw_grid,4) #We need to make like a quilt of values of the blocks and then search for stations that overlay a block
+     blocks[0] = np.zeros(blocks[0].shape)+1
+     blocks[1] = np.zeros(blocks[1].shape)+5
+     blocks[2] = np.zeros(blocks[2].shape)+9
+     blocks[3] = np.zeros(blocks[3].shape)+13
+     blocks[0] = [x.T for x in np.array_split(blocks[0].T, 4)] #transpose the array so we can split by column
+     blocks[0][1] = np.zeros(blocks[0][1].shape)+2
+     blocks[0][2] = np.zeros(blocks[0][2].shape)+3
+     blocks[0][3] = np.zeros(blocks[0][3].shape)+4
+     blocks[1] = [x.T for x in np.array_split(blocks[1].T, 4)]
+     blocks[1][1] = np.zeros(blocks[1][1].shape)+6
+     blocks[1][2] = np.zeros(blocks[1][2].shape)+7
+     blocks[1][3] = np.zeros(blocks[1][3].shape)+8
+     blocks[2] = [x.T for x in np.array_split(blocks[2].T, 4)]
+     blocks[2][1] = np.zeros(blocks[2][1].shape)+10
+     blocks[2][2] = np.zeros(blocks[2][2].shape)+11
+     blocks[2][3] = np.zeros(blocks[2][3].shape)+12
+     blocks[3] = [x.T for x in np.array_split(blocks[3].T, 4)]
+     blocks[3][1] = np.zeros(blocks[3][1].shape)+14
+     blocks[3][2] = np.zeros(blocks[3][2].shape)+15
+     blocks[3][3] = np.zeros(blocks[3][3].shape)+16
+     
+     #Ok now we knit it back together lengthwise
+     topBlock = np.concatenate([blocks[0][0],blocks[0][1],blocks[0][2],blocks[0][3]],axis=1)
+     secondBlock = np.concatenate([blocks[1][0],blocks[1][1],blocks[1][2],blocks[1][3]],axis=1)
+     thirdBlock = np.concatenate([blocks[2][0],blocks[2][1],blocks[2][2],blocks[2][3]],axis=1)
+     bottomBlock = np.concatenate([blocks[3][0],blocks[3][1],blocks[3][2],blocks[3][3]],axis=1)
+     #Now widthwise
+     blocks = np.concatenate([topBlock,secondBlock,thirdBlock,bottomBlock],axis=0)
+
+     return blocks
+
+def sorting_stations(blocks,shapefile,Cvar_dict):
+     '''Here we are sorting the stations based on their position on the array. 
+     '''
+     x_origin_list = []
+     y_origin_list = [] 
+
+     groups = {} 
+     station_name_list = []
+     projected_lat_lon = {}
+
+     for station_name in Cvar_dict.keys():
+          
+          if station_name in latlon_dict.keys():
+               
+             station_name_list.append(station_name)
+
+             loc = latlon_dict[station_name]
+             latitude = loc[0]
+             longitude = loc[1]
+             Plat, Plon = pyproj.Proj('esri:102001')(longitude,latitude)
+             Plat = float(Plat)
+             Plon = float(Plon)
+             projected_lat_lon[station_name] = [Plat,Plon]
+
+
+
+     for station_name_hold_back in station_name_list:
+        
+        na_map = gpd.read_file(shapefile)
+        bounds = na_map.bounds
+        xmax = bounds['maxx']
+        xmin= bounds['minx']
+        ymax = bounds['maxy']
+        ymin = bounds['miny']
+        pixelHeight = 10000 
+        pixelWidth = 10000
+
+        #Delete at a certain point
+        coord_pair = projected_lat_lon[station_name_hold_back]
+
+        x_orig = int((coord_pair[0] - float(bounds['minx']))/pixelHeight) #lon 
+        y_orig = int((coord_pair[1] - float(bounds['miny']))/pixelWidth) #lat
+
+        group = blocks[y_orig][x_orig] 
+        groups[station_name_hold_back] = group 
+
+     return groups
+
+def select_random_station(groups):
+     stations_selected = {} 
+     for group in range(1,16):
+          try: 
+               group1 = [k for k,v in groups.items() if v == group]
+               group1_selection = np.random.choice(group1,1)
+               print('Group selection %s is: %s'%(group,group1_selection[0]))
+               stations_selected[group] = group1_selection 
+          except ValueError: #No stations in that group!
+               print('No stations in group %s'%group)
+
+     return stations_selected
