8ee27b1e63d7afac04fa0f2ac1d02de3cb96555c
clara-risk <43723728+clara-risk@users.noreply.github.com>
2020-08-27 15:11:06 -0400

    add shuffle-split xval function

--- fire_weather_interpolate/tps.py
+++ fire_weather_interpolate/tps.py
@@ -23,6 +23,9 @@ import scipy.interpolate as interpolate
 import warnings
 warnings.filterwarnings("ignore") #Runtime warning suppress, this suppresses the /0 warning
 
+from sklearn.model_selection import ShuffleSplit
+
+
 #functions 
 def TPS(latlon_dict,Cvar_dict,input_date,var_name,shapefile,show,phi):
     '''Thin plate splines interpolation implemented using the interpolate radial basis function from 
@@ -298,3 +301,153 @@ def cross_validate_tps(latlon_dict,Cvar_dict,shapefile,phi):
 
     return absolute_error_dictionary
 
+def shuffle_split_tps(latlon_dict,Cvar_dict,shapefile,phi,rep):
+    '''Shuffle-split cross-validation for thin plate splines 
+    Parameters
+        latlon_dict (dict): the latitude and longitudes of the hourly stations, loaded from the 
+        .json file
+        Cvar_dict (dict): dictionary of weather variable values for each station 
+        shapefile (str): path to the study area shapefile 
+        phi (float): smoothing parameter for the thin plate spline, if 0 no smoothing
+        rep (int): number of replications 
+    Returns 
+        overall_error (float): MAE average of all the replications
+     '''
+    count = 1
+    error_dictionary = {}
+    while count <= rep:
+        x_origin_list = []
+        y_origin_list = [] 
+        z_origin_list = []
+        absolute_error_dictionary = {} #for plotting
+        station_name_list = []
+        projected_lat_lon = {}
+
+        for station_name in Cvar_dict.keys():
+            if station_name in latlon_dict.keys():
+                station_name_list.append(station_name)
+
+                loc = latlon_dict[station_name]
+                latitude = loc[0]
+                longitude = loc[1]
+                Plat, Plon = pyproj.Proj('esri:102001')(longitude,latitude)
+                Plat = float(Plat)
+                Plon = float(Plon)
+                projected_lat_lon[station_name] = [Plat,Plon]
+
+
+        for station_name_hold_back in station_name_list:
+
+            na_map = gpd.read_file(shapefile)
+            bounds = na_map.bounds
+
+            pixelHeight = 10000 
+            pixelWidth = 10000
+
+
+            coord_pair = projected_lat_lon[station_name_hold_back]
+
+            x_orig = int((coord_pair[0] - float(bounds['minx']))/pixelHeight) #lon 
+            y_orig = int((coord_pair[1] - float(bounds['miny']))/pixelWidth) #lat
+            x_origin_list.append(x_orig)
+            y_origin_list.append(y_orig)
+            z_origin_list.append(Cvar_dict[station_name_hold_back])
+
+
+        #Split the stations in two
+        stations = np.array(list(Cvar_dict.keys()))
+        splits = ShuffleSplit(n_splits=1, train_size=.5) #Won't be exactly 50/50 if uneven num stations
+
+        for train_index, test_index in splits.split(stations):
+
+               train_stations = stations[train_index] 
+               #print(train_stations)
+               test_stations = stations[test_index]
+               #print(test_stations)
+
+          #They can't overlap
+
+          for val in train_stations:
+               if val in test_stations:
+                    print('Error, the train and test sets overlap!')
+                    sys.exit()
+
+        lat = []
+        lon = []
+        Cvar = []
+        for station_name in sorted(Cvar_dict.keys()):
+            if station_name in latlon_dict.keys():
+                if station_name not in train_stations:
+                    loc = latlon_dict[station_name]
+                    latitude = loc[0]
+                    longitude = loc[1]
+                    cvar_val = Cvar_dict[station_name]
+                    lat.append(float(latitude))
+                    lon.append(float(longitude))
+                    Cvar.append(cvar_val)
+                else:
+                    pass
+                    
+        y = np.array(lat)
+        x = np.array(lon)
+        z = np.array(Cvar) 
+
+        na_map = gpd.read_file(shapefile)
+        bounds = na_map.bounds
+        xmax = bounds['maxx']
+        xmin= bounds['minx']
+        ymax = bounds['maxy']
+        ymin = bounds['miny']
+        pixelHeight = 10000 
+        pixelWidth = 10000
+                
+        num_col = int((xmax - xmin) / pixelHeight)
+        num_row = int((ymax - ymin) / pixelWidth)
+
+
+        #We need to project to a projected system before making distance matrix
+        source_proj = pyproj.Proj(proj='latlong', datum = 'NAD83') #We dont know but assume 
+        xProj, yProj = pyproj.Proj('esri:102001')(x,y)
+
+        yProj_extent=np.append(yProj,[bounds['maxy'],bounds['miny']])
+        xProj_extent=np.append(xProj,[bounds['maxx'],bounds['minx']])
+
+        Yi = np.linspace(np.min(yProj_extent),np.max(yProj_extent),num_row)
+        Xi = np.linspace(np.min(xProj_extent),np.max(xProj_extent),num_col)
+
+        Xi,Yi = np.meshgrid(Xi,Yi)
+
+        empty_grid = np.empty((num_row,num_col,))*np.nan
+
+        for x,y,z in zip(x_origin_list,y_origin_list,z_origin_list):
+            empty_grid[y][x] = z
+
+
+
+        vals = ~np.isnan(empty_grid)
+
+        func = interpolate.Rbf(Xi[vals],Yi[vals],empty_grid[vals], function='thin_plate',smooth=phi)
+        thin_plate = func(Xi,Yi)
+        spline = thin_plate.reshape(num_row,num_col)
+
+        #Calc the RMSE, MAE, at the pixel loc
+        #Delete at a certain point
+        for station_name_hold_back in train_stations: 
+            coord_pair = projected_lat_lon[station_name_hold_back]
+
+            x_orig = int((coord_pair[0] - float(bounds['minx']))/pixelHeight) #lon 
+            y_orig = int((coord_pair[1] - float(bounds['miny']))/pixelWidth) #lat
+            x_origin_list.append(x_orig)
+            y_origin_list.append(y_orig)
+
+            interpolated_val = spline[y_orig][x_orig] 
+
+            original_val = Cvar_dict[station_name]
+            absolute_error = abs(interpolated_val-original_val)
+            absolute_error_dictionary[station_name_hold_back] = absolute_error
+        error_dictionary[count]= sum(absolute_error_dictionary.values())/len(absolute_error_dictionary.values()) #average of all the withheld stations
+        count+=1
+
+    overall_error = sum(error_dictionary.values())/rep
+    
+    return absolute_error_dictionary
