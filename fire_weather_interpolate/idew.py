4ee263b5c18ad0f767f947e9c329bd85d9b0a43e
clara-risk <43723728+clara-risk@users.noreply.github.com>
2020-08-28 12:59:12 -0400

    Fix the missing station problem for rainfall

--- fire_weather_interpolate/idew.py
+++ fire_weather_interpolate/idew.py
@@ -369,8 +369,12 @@ def shuffle_split_IDEW(latlon_dict,Cvar_dict,shapefile,file_path_elev,elev_array
         station_name_list = []
         projected_lat_lon = {}
 
+        stations_input = [] #we can't just use Cvar_dict.keys() because some stations do not have valid lat/lon
+        for station_code in Cvar_dict.keys():
+            if station_code in loc_dict.keys():
+                stations_input.append(station_code)
         #Split the stations in two
-        stations = np.array(list(Cvar_dict.keys()))
+        stations = np.array(stations_input)
         splits = ShuffleSplit(n_splits=1, train_size=.5) #Won't be exactly 50/50 if uneven num stations
 
         for train_index, test_index in splits.split(stations):
