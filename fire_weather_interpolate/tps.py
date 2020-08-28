3c4b5502203b4165d572cbcfebb3e07ff9cd1e73
clara-risk <43723728+clara-risk@users.noreply.github.com>
2020-08-28 12:57:48 -0400

    Fix the missing station problem for rainfall

--- fire_weather_interpolate/tps.py
+++ fire_weather_interpolate/tps.py
@@ -355,7 +355,12 @@ def shuffle_split_tps(latlon_dict,Cvar_dict,shapefile,phi,rep):
 
 
         #Split the stations in two
-        stations = np.array(list(Cvar_dict.keys()))
+        stations_input = [] #we can't just use Cvar_dict.keys() because some stations do not have valid lat/lon
+        for station_code in Cvar_dict.keys():
+            if station_code in loc_dict.keys():
+                stations_input.append(station_code)
+          #Split the stations in two
+        stations = np.array(stations_input)
         splits = ShuffleSplit(n_splits=1, train_size=.5) #Won't be exactly 50/50 if uneven num stations
 
         for train_index, test_index in splits.split(stations):
