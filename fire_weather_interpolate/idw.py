49052c4180b3e5bab73d06e085b7d7b77d9d4628
clara-risk <clara.risk@mail.utoronto.ca>
2020-11-29 14:08:40 -0500

    fix version

--- fire_weather_interpolate/idw.py
+++ fire_weather_interpolate/idw.py
@@ -7,6 +7,7 @@ Code for interpolating weather data using inverse distance weighting interpolati
 """
     
 #import
+
 import geopandas as gpd
 import numpy as np
 import pyproj
@@ -44,6 +45,15 @@ def IDW(latlon_dict,Cvar_dict,input_date,var_name,shapefile,show,d):
      lat = []
      lon = []
      Cvar = []
+     
+     source_proj = pyproj.Proj(proj='latlong', datum = 'NAD83')
+     na_map = gpd.read_file(shapefile)
+     bounds = na_map.bounds
+     xmax = bounds['maxx']+200000 
+     xmin= bounds['minx']-200000 
+     ymax = bounds['maxy']+200000 
+     ymin = bounds['miny']-200000
+     
      for station_name in Cvar_dict.keys():
 
         if station_name in latlon_dict.keys():
@@ -51,20 +61,18 @@ def IDW(latlon_dict,Cvar_dict,input_date,var_name,shapefile,show,d):
             loc = latlon_dict[station_name]
             latitude = loc[0]
             longitude = loc[1]
-            cvar_val = Cvar_dict[station_name]
-            lat.append(float(latitude))
-            lon.append(float(longitude))
-            Cvar.append(cvar_val)
+            proj_coord = pyproj.Proj('esri:102001')(longitude,latitude) #Filter out stations outside of grid
+            if (proj_coord[1] <= float(ymax[0]) and proj_coord[1] >= float(ymin[0]) and proj_coord[0] <= float(xmax[0]) and proj_coord[0] >= float(xmin[0])):
+                 cvar_val = Cvar_dict[station_name]
+                 lat.append(float(latitude))
+                 lon.append(float(longitude))
+                 Cvar.append(cvar_val)
+            else:
+                 print(station_name) 
      y = np.array(lat)
      x = np.array(lon)
      z = np.array(Cvar) 
 
-     na_map = gpd.read_file(shapefile)
-     bounds = na_map.bounds
-     xmax = bounds['maxx']
-     xmin= bounds['minx']
-     ymax = bounds['maxy']
-     ymin = bounds['miny']
      pixelHeight = 10000 
      pixelWidth = 10000
             
@@ -73,11 +81,11 @@ def IDW(latlon_dict,Cvar_dict,input_date,var_name,shapefile,show,d):
 
 
      #We need to project to a projected system before making distance matrix
-     source_proj = pyproj.Proj(proj='latlong', datum = 'NAD83') 
      xProj, yProj = pyproj.Proj('esri:102001')(x,y)
+               
 
-     yProj_extent=np.append(yProj,[bounds['maxy'],bounds['miny']])
-     xProj_extent=np.append(xProj,[bounds['maxx'],bounds['minx']])
+     yProj_extent=np.append(yProj,[bounds['maxy']+200000,bounds['miny']-200000])
+     xProj_extent=np.append(xProj,[bounds['maxx']+200000,bounds['minx']-200000])
 
      Yi = np.linspace(np.min(yProj_extent),np.max(yProj_extent),num_row)
      Xi = np.linspace(np.min(xProj_extent),np.max(xProj_extent),num_col)
