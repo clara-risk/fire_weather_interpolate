72f2c6faae74acd824e435a0be33581c0e061c56
clara-risk <crisk@wisc.edu>
2021-02-18 15:56:24 -0500

    create run_comparison

--- fire_weather_interpolate/auto_select.py
+++ fire_weather_interpolate/auto_select.py
@@ -7,3 +7,11 @@ Auto-selection protocol for spatial interpolation methods using shuffle-split cr
 References
 ----------
 """
+
+def run_comparison(interpolation_types):
+     '''Execute the shuffle-split cross-validation for the given interpolation types 
+     Parameters
+         interpolation_types (list of str): list of interpolation types to consider
+     Returns 
+         interpolation_best (str): returns the selected interpolation type name 
+     '''    
