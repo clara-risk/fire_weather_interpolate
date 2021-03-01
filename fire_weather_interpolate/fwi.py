08ae9820261cab14feb780fc045fa81e25300ea8
clara-risk <crisk@wisc.edu>
2021-03-01 16:05:38 -0500

    changes to plotting for june/july

--- fire_weather_interpolate/fwi.py
+++ fire_weather_interpolate/fwi.py
@@ -2462,6 +2462,9 @@ def plot_july(fwi_list,maxmin,year,var,shapefile,shapefile2):
                        ,vmin=0,vmax=maxval, clip_path=circ, clip_on=True,origin='upper')
         na_map.plot(ax = ax[0,0],facecolor="none",edgecolor='k',linewidth=1)
 
+        ax.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+        ax.ticklabel_format(useOffset=False, style='plain')
+
         ax.set_title(title)
         ax.invert_yaxis()
 
@@ -2527,6 +2530,9 @@ def plot_june(fwi_list,maxmin,year,var,shapefile,shapefile2):
         im = ax.imshow(fwi_list[index],extent=(min_xProj_extent-1,max_xProj_extent+1,max_yProj_extent-1,min_yProj_extent+1)\
                        ,vmin=0,vmax=maxval, clip_path=circ, clip_on=True,origin='upper')
         na_map.plot(ax = ax[0,0],facecolor="none",edgecolor='k',linewidth=1)
+
+        ax.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+        ax.ticklabel_format(useOffset=False, style='plain')
 
         ax.set_title(title)
         ax.invert_yaxis()
