61441e0e92a2cfbda154f2819f4e00f44727992c
clara-risk <clara.risk@mail.utoronto.ca>
2021-04-21 11:47:42 -0400

    pep8

--- fire_weather_interpolate/idew.py
+++ fire_weather_interpolate/idew.py
@@ -905,18 +905,35 @@ def select_block_size_IDEW(nruns, group_type, loc_dict, Cvar_dict, idw_example_g
     return lowest_stdev, ave_MAE
 
 
-def spatial_groups_IDEW(idw_example_grid, loc_dict, Cvar_dict, shapefile, d, blocknum, nfolds, replacement, dictionary_Groups, file_path_elev, idx_list, elev_array):
-    '''Spatially blocked bagging cross-validation procedure for IDW
+def spatial_groups_IDEW(idw_example_grid, loc_dict, Cvar_dict, shapefile, d, blocknum, nfolds, replacement, dictionary_Groups,
+                        file_path_elev, idx_list, elev_array):
+    '''Stratified shuffle-split cross-validation procedure
     Parameters
-    idw_example_grid (numpy array): the example idw grid to base the size of the group array off of
-    loc_dict (dict): the latitude and longitudes of the hourly stations, loaded from the
-    .json file
-    Cvar_dict (dict): dictionary of weather variable values for each station
-    shapefile (str): path to the study area shapefile
-    d (int): the weighting function for IDW interpolation
-    nfolds (int): # number of folds. For 10-fold we use 10, etc.
+    ----------
+         idw_example_grid  : ndarray
+              used for reference of study area grid size
+         loc_dict : dictionary
+              the latitude and longitudes of the daily/hourly stations
+         Cvar_dict : dictionary
+              dictionary of weather variable values for each station
+         shapefile : string
+              path to the study area shapefile
+         d : int
+              the weighting for IDW interpolation
+         blocknum : int
+              number of blocks/clusters
+         nfolds : int
+              number of folds to create (essentially repetitions)
+         replacement : bool
+              whether or not to use replacement between folds, should usually be true
+         dictionary_Groups : dictionary
+              dictionary of what groups (clusters) the stations belong to
+         elev_array : ndarray
+             array for elevation, create using IDEW interpolation (this is a trick to speed up code)
     Returns
-    error_dictionary (dict): a dictionary of the absolute error at each fold when it was left out
+    ----------
+         dictionary
+              - a dictionary of the absolute error at each fold when it was left out
     '''
     station_list_used = []  # If not using replacement, keep a record of what we have done
     count = 1
